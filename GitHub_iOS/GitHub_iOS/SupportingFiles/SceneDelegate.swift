37ff0781c83b946ae592d28ca634b07f0224369d
전소영 <jsu3417@gmail.com>
2022-08-07 20:37:20 +0900

    SceneDelegate login 로직 추가

--- GitHub_iOS/GitHub_iOS/SupportingFiles/SceneDelegate.swift
+++ GitHub_iOS/GitHub_iOS/SupportingFiles/SceneDelegate.swift
@@ -6,46 +6,78 @@
 //
 
 import UIKit
+import RxSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    let tabBarController = TabBarController()
+    private let disposeBag = DisposeBag()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let tabBarController = TabBarController()
         window?.rootViewController = tabBarController
+        self.checkLoginState()
         window?.makeKeyAndVisible()
     }
+    
+    private func checkLoginState() {
+        // FIXME: isLogin 상태로
+        if KeychainManager.shared.readAccessToken(key: "accessToken") != nil {
+            tabBarController.setupNavigationBarRightButtonItem(size: (28, 28), imageName: "logout")
+        } else {
+            tabBarController.setupNavigationBarRightButtonItem(size: (28, 28), imageName: "login")
+        }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let code = URLContexts.first?.url.absoluteString.components(separatedBy: "code=").last else {
+            tabBarController.showToast(message: "로그인 재시도 바랍니다.")
+            return
+        }
 
+        LoginManager.shared.login(with: code)
+            .subscribe(onNext: { [weak self] result in
+                switch result {
+                case .success():
+                    NotificationCenter.default.post(name: .loginSuccess, object: nil)
+                case .failure(let error):
+                    print(error)
+                    self?.tabBarController.showToast(message: "로그인 재시도 바랍니다.")
+                    break
+                }
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
