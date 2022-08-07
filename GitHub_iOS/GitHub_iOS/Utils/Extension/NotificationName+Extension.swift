c87986cd929739e40f41779044dd9e314bc729bb
전소영 <jsu3417@gmail.com>
2022-08-07 20:32:58 +0900

    NotificationName+Extension 추가

---
+++ GitHub_iOS/GitHub_iOS/Utils/Extension/NotificationName+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName+Extension.swift
+//  GitHub_iOS
+//
+//  Created by 전소영 on 2022/08/07.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let loginSuccess = NSNotification.Name("loginSuccess")
+    static let logoutSuccess = NSNotification.Name("logoutSuccess")
+}
