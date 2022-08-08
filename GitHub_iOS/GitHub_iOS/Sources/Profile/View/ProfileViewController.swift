1290cce9a29f2070c1418d4c33ea230dd8d49ef1
전소영 <jsu3417@gmail.com>
2022-08-08 22:20:15 +0900

    ProfileViewController tableView 추가

--- GitHub_iOS/GitHub_iOS/Sources/Profile/View/ProfileViewController.swift
+++ GitHub_iOS/GitHub_iOS/Sources/Profile/View/ProfileViewController.swift
@@ -14,15 +14,15 @@ import RxSwift
 class ProfileViewController: UIViewController {
     private let disposeBag = DisposeBag()
     
-    private let label = UILabel().then {
-        $0.text = "profile"
-        $0.font = UIFont.setFont(type: .bold, size: 32)
+    private lazy var myStarRepositoryTableView = UITableView(frame: CGRect.zero, style: .grouped).then {
+        $0.backgroundColor = .white
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupNotification()
+        setupTableView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,12 +39,16 @@ class ProfileViewController: UIViewController {
     }
     
     private func setupSubViews() {
-        view.addSubviews([label])
+        view.addSubview(myStarRepositoryTableView)
     }
     
     private func setupConstraints() {
-        label.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+        guard let tabBarSize = tabBarController?.tabBar.frame.size.height else {
+            return
+        }
+        myStarRepositoryTableView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(tabBarSize)
         }
     }
     
@@ -54,5 +58,60 @@ class ProfileViewController: UIViewController {
                 print("logout noti 수신")
                 self?.navigationController?.pushViewController(LoginViewController(), animated: false)
             }).disposed(by: disposeBag)
+    }
+    
+    private func setupTableView() {
+        myStarRepositoryTableView.dataSource = self
+        myStarRepositoryTableView.delegate = self
+        
+        myStarRepositoryTableView.showsVerticalScrollIndicator = false
+        myStarRepositoryTableView.registerCell(cellType: RepositoryTableViewCell.self)
+        myStarRepositoryTableView.register(MyStarRepositoryTableViewHeaderView.self, forHeaderFooterViewReuseIdentifier: MyStarRepositoryTableViewHeaderView.headerViewID)
+    }
+}
+
+// MARK: UITableViewDataSource
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //FIXME: viewModel data 로 변경
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let repositoryTableViewCell = tableView.dequeueReusableCell(cellType: RepositoryTableViewCell.self, indexPath: indexPath) else {
+            return UITableViewCell()
+        }
+        repositoryTableViewCell.setupUI(index: indexPath.row)
+        return repositoryTableViewCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+}
+
+// MARK: UITableViewDelegate
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let myStarRepositoryTableViewHeaderView = tableView.dequeueReusableHeaderFooterView(withIdentifier: MyStarRepositoryTableViewHeaderView.headerViewID) as? MyStarRepositoryTableViewHeaderView else {
+            //TODO: myStarRepositoryTableViewHeaderView UI udate
+            //            myStarRepositoryTableViewHeaderView.userIDLabel
+            
+            return UIView()
+        }
+        
+        return myStarRepositoryTableViewHeaderView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return section == 0 ? 132 : .leastNonzeroMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        .leastNonzeroMagnitude
     }
 }
