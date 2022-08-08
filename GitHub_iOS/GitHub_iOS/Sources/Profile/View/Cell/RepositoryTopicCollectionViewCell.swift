5dd96ba30f71bb2dbb028b2cf84f58722061410c
전소영 <jsu3417@gmail.com>
2022-08-08 22:02:09 +0900

    RepositoryTopicCollectionViewCell 추가

---
+++ GitHub_iOS/GitHub_iOS/Sources/Profile/View/Cell/RepositoryTopicCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  RepositoryTopicCollectionViewCell.swift
+//  GitHub_iOS
+//
+//  Created by 전소영 on 2022/08/08.
+//
+
+import UIKit
+
+class RepositoryTopicCollectionViewCell: UICollectionViewCell {
+    private let repositoryTopicLabelView = UIView().then {
+        $0.backgroundColor = .mainSkyBlue
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 4
+    }
+    
+    private let repositoryTopicLabel = UILabel().then {
+        $0.textColor = .mainBlue
+        $0.font = UIFont.setFont(type: .medium, size: 12)
+        $0.textAlignment = .center
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    private func setupView() {
+        self.addSubview(repositoryTopicLabelView)
+        repositoryTopicLabelView.addSubview(repositoryTopicLabel)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        repositoryTopicLabelView.snp.makeConstraints {
+            $0.centerX.centerY.width.equalToSuperview()
+            $0.height.equalTo(14)
+        }
+        
+        repositoryTopicLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+   
+    func setupUI(topic: String) {
+        repositoryTopicLabel.text = topic
+    }
+}
