8dd32a934a3aabc4896a95da58e5bc3061a2220b
전소영 <jsu3417@gmail.com>
2022-08-10 13:27:09 +0900

    MyStarRepositoryTableViewHeaderView setupUI 추가

--- GitHub_iOS/GitHub_iOS/Sources/Profile/View/MyStarRepositoryTableViewHeaderView.swift
+++ GitHub_iOS/GitHub_iOS/Sources/Profile/View/MyStarRepositoryTableViewHeaderView.swift
@@ -9,49 +9,58 @@ import UIKit
 
 final class MyStarRepositoryTableViewHeaderView: UITableViewHeaderFooterView {
     static let headerViewID = "MyStarRepositoryTableViewHeaderView"
-
+    
     private let userImageView = UIImageView().then {
-        //FIXME: test 후 지우기
-        $0.image = UIImage(named: "testImage")
+        $0.contentMode = .scaleToFill
         $0.layer.cornerRadius = 45
         $0.layer.masksToBounds = true
+        $0.addBorder(color: .lightGray, width: 0.5)
+        $0.addShadow()
     }
-
+    
     private let userIDLabel = UILabel().then {
-        $0.text = "userID"
         $0.font = UIFont.setFont(type: .medium, size: 28)
     }
-
+    
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         setupView()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
     }
-
+    
     private func setupView() {
         self.backgroundColor = .white
         setupSubViews()
         setupConstraints()
     }
-
+    
     private func setupSubViews() {
         self.addSubviews([userImageView, userIDLabel])
     }
-
+    
     private func setupConstraints() {
         userImageView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(16)
             $0.leading.equalToSuperview().inset(24)
             $0.width.height.equalTo(90)
         }
-
+        
         userIDLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.leading.equalTo(userImageView.snp.trailing).offset(24)
+        }
+    }
+    
+    func setupUI(data: [String: String]) {
+        userIDLabel.text = data.keys.first
+        if let userImageURL = data.values.first {
+            userImageView.kf.setImage(with: URL(string: userImageURL))
+        } else {
+            userImageView.image = UIImage(named: "defaultUser")
         }
     }
 }
