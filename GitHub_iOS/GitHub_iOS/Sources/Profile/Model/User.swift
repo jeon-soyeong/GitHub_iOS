2a8d20d6f616859cf9bf52ea527334bf25978442
전소영 <jsu3417@gmail.com>
2022-08-10 12:50:58 +0900

    User, UserRepository model 추가

---
+++ GitHub_iOS/GitHub_iOS/Sources/Profile/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  GitHub_iOS
+//
+//  Created by 전소영 on 2022/08/09.
+//
+
+import Foundation
+
+struct User: Codable {
+    let userID: String
+    let userImageURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userID = "login"
+        case userImageURL = "avatar_url"
+    }
+}
